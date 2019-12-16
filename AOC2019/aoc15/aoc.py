c127a7ab459178e61325e3d2ff567e9d39c2a407
Julius Parulek <parulek@gmail.com>
2019-12-16 01:43:22 +0100

    fix aoc15, part2 missing yet

--- AOC2019/aoc15/aoc.py
+++ AOC2019/aoc15/aoc.py
@@ -45,9 +45,12 @@ class Amp:
         self.index = 0
         self.output_count = 0
         self.data = [0, 0, 0]
-        self.droid = (15, 15)
+        self.droid = (30, 30)
         self.board = {}
         self.input = 0
+        self.oxygen = None
+        self.length = 0
+        self.arr = {}
 
     def get_pos(self, input):
 
@@ -69,13 +72,81 @@ class Amp:
     def update_stats(self):
         id = (self.data[0], self.data[1], )
 
+    def check_board(self):
+        for k in self.board:
+            if self.board[k] ==  MOVED_OK:
+                return False
+            return True
+
+    def compute_oxygen(self, x, y, t):
+        id = (x, y)
+        if id in self.arr:
+            return 10000000
+        if id in self.board:
+            self.arr[id] = t
+            if id == self.oxygen:
+                return 10000000
+            elif self.board[id] == WALL:
+                return 10000000
+            else:
+                self.board[id] = 10
+                if self.check_board():
+                    return t
+
+            a = self.compute_length(x + 1, y, t + 1)
+            b = self.compute_length(x - 1, y, t + 1)
+            c = self.compute_length(x   , y + 1, t + 1)
+            d = self.compute_length(x   , y - 1, t + 1)
+            return min(a,min(b,min(c,d)))
+        else:
+            return 10000000000
+
+    def compute_length(self, x, y, l):
+        id = (x, y)
+        if id in self.arr:
+            return 10000000
+        if id in self.board:
+            self.arr[id] = l
+            if id == self.oxygen:
+                print('Current l=',l)
+                return l
+            elif self.board[id] == WALL:
+                return 10000000
+            a = self.compute_length(x + 1, y, l + 1)
+            b = self.compute_length(x - 1, y, l + 1)
+            c = self.compute_length(x   , y + 1, l + 1)
+            d = self.compute_length(x   , y - 1, l + 1)
+            return min(a,min(b,min(c,d)))
+        else:
+            return 10000000000
+        
+
+    def get_distance(self, _input):
+        l = 0
+        pos = self.start
+        while True:
+            p = _input[pos]
+            if pos == self.oxygen:
+                return l 
+            dir = {
+                NORTH: (0,1),
+                SOUTH: (0,-1),
+                WEST: (-1,0),
+                EAST: (1, 0)
+            }
+            pos = (pos[0] + dir[p][0],pos[1] + dir[p][1])
+            l += 1
+
+
+
     def draw_board(self):
-        a = np.ones((30, 30), dtype=np.int32)
+        a = np.ones((2*self.start[0], 2*self.start[1]), dtype=np.int32)
         for id in self.board:
             a[id] = self.board[id]
         a[self.droid] = 3
         a[self.start] = 5
-        a[self.oxygen] = 4
+        if self.oxygen is not None:
+            a[self.oxygen] = 4
         _d = {
             WALL: '#',
             MOVED_OK: '.',
@@ -160,7 +231,7 @@ class Amp:
                 self.index += 4
             elif operator == 3:
                 self.input = _inputs[self.droid]
-                print('SENDING INPUT {}'.format(_inputs))
+                # print('SENDING INPUT {}'.format(_inputs))
                 store_output(1, self.input, mode_a)
                 self.index += 2
             elif operator == 4:
@@ -176,13 +247,22 @@ class Amp:
                             _inputs[self.droid] = NORTH
                     else:
                         _inputs[self.droid] = NORTH
+                    self.length += 1
+                    if self.droid == self.start:
+                        self.length = 0
                 else:
-                    print('YESSSSSSSSSSSSS!!!!!')
+                    
                     self.oxygen = self.droid
                     self.draw_board()
+                    print('YES.... ', self.length)
+                    # l = self.get_distance(_inputs)
+                    l = self.compute_length(self.start[0], self.start[1], 0)
+                    print('start to oxygen=', l)
+                    l = self.compute_oxygen(self.oxygen[0], self.oxygen[1], 0)
+                    print('time to re-oxygen=', l)
                     return True
                 # print('DROID_POS: ', self.droid)
-                self.draw_board()
+                # self.draw_board()
                 # output.append(a)
                 self.index += 2
             elif operator == 5:
@@ -209,8 +289,7 @@ class Amp:
                 raise Exception('Something Went wrong {} modes A:{} B:{}'.format(arr[self.index], mode_a, mode_b))
 
 a = Amp()
-# arr[0] = 2
-output, b = a.compute(arr)
+a.compute(arr)
 
 
 
