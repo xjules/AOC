f6454096e303f97d8f11f110adcf40b9ef4849d1
Julius Parulek <parulek@gmail.com>
2020-12-03 09:44:27 +0100

    Add new input processing function

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,12 @@ def read_lines_int(filename):
         l = f.readlines()
         return [int(a) for a in l]
     return None
+
+
+def read_lines_str(filename, ch=None):
+    with open(filename) as f:
+        l = f.readlines()
+        if bool(ch):
+            return [a.split(ch) for a in l]
+        return l
+    return None
