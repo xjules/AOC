117d0ac0baaa6402b7bf5698e51bdd410b34fb95
Julius Parulek <parulek@gmail.com>
2022-12-14 22:28:25 +0100

    Add AOC2022/aoc14/aoc.py

--- AOC2022/aoc14/aoc.py
+++ AOC2022/aoc14/aoc.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from utils import read_lines_str
 
-values = read_lines_str("AOC2022/aoc14/test_input.txt")
+values = read_lines_str("AOC2022/aoc14/input.txt")
 
 
 def get_line(a, b):
@@ -26,15 +26,32 @@ ll = []
 
 
 def do_sand(x, y, g, max_y):
+    # print(f"({x=},{y=})")
     if y >= max_y:
         return False
     if g[x, y + 1] == 0:
-        do_sand(x, y + 1, g, max_y)
+        return do_sand(x, y + 1, g, max_y)
     elif g[x - 1, y + 1] == 0:
-        do_sand(x - 1, y + 1, g, max_y)
+        return do_sand(x - 1, y + 1, g, max_y)
     elif g[x + 1, y + 1] == 0:
-        do_sand(x + 1, y + 1, g, max_y)
+        return do_sand(x + 1, y + 1, g, max_y)
     else:
+        g[x, y] = 9
+    return True
+
+
+def do_sand_part2(x, y, g):
+    # print(f"({x=},{y=})")
+    if g[x, y + 1] == 0:
+        return do_sand_part2(x, y + 1, g)
+    elif g[x - 1, y + 1] == 0:
+        return do_sand_part2(x - 1, y + 1, g)
+    elif g[x + 1, y + 1] == 0:
+        return do_sand_part2(x + 1, y + 1, g)
+    else:
+        if x == 500 and y == 0:
+            print("DONE")
+            return False
         g[x, y] = 9
     return True
 
@@ -52,7 +69,13 @@ for line in values:
 min_x = np.min([a.min(0) for a in ll], 0)
 max_x = np.max([a.max(0) for a in ll], 0)
 
-grid = np.zeros((510, 10))
+# part 1
+# grid = np.zeros((max_x[0] + 1, max_x[1] + 2))
+# part 2
+
+grid = np.zeros((20 * max_x[0], max_x[1] + 3))
+grid[:, max_x[1] + 2] = 1
+
 
 # draw lines
 for l in ll:
@@ -62,11 +85,16 @@ for l in ll:
         a = b
 
 
+print(grid[490:510, :].T)
 balls = 0
 while True:
-    if not do_sand(500, 0, grid, max_x[1]):
+    print(f"doing {balls=}")
+    # if not do_sand(500, 0, grid, max_x[1]) or balls > 22:
+    if not do_sand_part2(500, 0, grid):
+        balls += 1
         print(f"{balls=}")
+
         break
     balls += 1
-print(grid[490:, :].T)
+# print(grid[490:, :].T)
 # get sand
